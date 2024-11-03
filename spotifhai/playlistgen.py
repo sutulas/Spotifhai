66d9ee2705906b154c9723018a2c5b246a5729ed
mowenista <ostannard12@gmail.com>
2024-11-03 17:25:24 -0500

    thinking through playlist generation

---
+++ spotifhai/playlistgen.py
@@ -0,0 +1,8 @@
+# Steps for AI playlist Generation
+
+# 1. an HTTP GET request to the /recommendations endpoint, to get the tracks;
+
+# 2. an HTTP POST request to the /playlists endpoint to create a new playlist;
+
+# 3. an HTTP POST request to the /playlist endpoint to add the songs.
+
