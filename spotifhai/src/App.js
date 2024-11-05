61254d6f92af4d73b71f1afa713429273b896c45
Seamus Sutula <sutulas@bc.edu>
2024-11-05 16:50:45 -0500

    Fixing refresh issue

--- spotifhai/src/App.js
+++ spotifhai/src/App.js
@@ -3,24 +3,23 @@ import axios from 'axios';
 import Login from './pages/Login';
 import Main from './pages/Main';
 
-
-
 const App = () => {
-  const [loggedIn, setLoggedIn] = React.useState(false);
+  const [loggedIn, setLoggedIn] = React.useState(
+    () => localStorage.getItem('loggedIn') === 'true' // Check initial login state from localStorage
+  );
 
-  
+  // Update localStorage whenever loggedIn changes
+  useEffect(() => {
+    localStorage.setItem('loggedIn', loggedIn);
+  }, [loggedIn]);
 
   if (!loggedIn) {
     return (
       <Login setLogin={() => setLoggedIn(true)} />
-    )
+    );
   }
-  return (<Main />)
+  
+  return <Main />;
 }
 
 export default App;
-
-
-
-
-// Potenial issue: Only one user at a time??? If i login on one browser, it auto logs-in on another browser
